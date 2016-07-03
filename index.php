1c7cef4af48cdc6b95397baeaf8bfb840c48a20e
Jeremiah Bohling <jbohling@sycamoreleaf.com>
2016-07-03 14:47:20 -0600

    Update example client to process new game rules

--- index.php
+++ index.php
@@ -1,16 +1,37 @@
 <?
     $board = json_decode($_POST['board']);
-    $done = false;
-    foreach($board as $x => $col) {
-        foreach($col as $y => $cell) {
-            if($cell == 0) {
-                $done = true;
-                break;
+
+    if($_POST['player'] == 2) {
+        foreach($board as $x => $col) {
+            foreach($col as $y => $cell) {
+                if($cell == 1) {
+                    $x1 = $x;
+                    $y1 = $y;
+                } elseif($cell == 2) {
+                    $x2 = $x;
+                    $y2 = $y;
+                }
             }
         }
-        if($done) break;
-    }
 
-    if($done) echo json_encode(array($x, $y));
-    else echo 'fail';
+        $dx = $x1 - $x2;
+        $dy = $y1 - $y2;
+
+        if(abs($dx) > abs($dy)) {
+            if($dx > 0) {
+                echo 2;
+            } else {
+                echo 0;
+            }
+        } else {
+            if($dy > 0) {
+                echo 3;
+            } else {
+                echo 1;
+            }
+        }
+
+    } else {
+        echo rand(0, 3);
+    }
 ?>
