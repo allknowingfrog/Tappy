4b78f2d63b34c594a4874996b1cde526eedc9da4
Jeremiah A. Bohling <jbohling@sycamoreleaf.com>
2016-07-03 00:56:33 -0500

    Basic proof of concept

--- app.js
+++ app.js
@@ -1,5 +1,60 @@
-var request = require("request");
+var request = require('request');
 
-request('file:///home/jeremiah/tappy/index.html', function(error, response, body) {
-    console.log(
-});
+var board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
+var url = 'http://www.jonhastheconch.com/tappy/index.php';
+
+function process() {
+    var data = {
+        url: url,
+        formData: {
+            board: JSON.stringify(board)
+        }
+    };
+
+    request.post(data, function(err, res, body) {
+        var move = JSON.parse(body);
+        var x = move[0];
+        var y = move[1];
+        if(board[x][y] == 0) {
+            board[x][y] = 1;
+            print();
+            if(!stop()) {
+                process();
+            }
+        }
+    });
+}
+
+function print() {
+    var output;
+    for(var y=0; y<board.length; y++) {
+        output = '';
+        for(var x=0; x<board[y].length; x++) {
+            output += board[x][y] + ' ';
+        }
+        console.log(output);
+    }
+    console.log('');
+}
+
+function stop() {
+    var done = true;
+    for(var y=0; y<board.length; y++) {
+        for(var x=0; x<board[y].length; x++) {
+            if(board[x][y] == 0) {
+                done = false;
+                break;
+            }
+        }
+        if(!done) break;
+    }
+
+    return done;
+}
+
+process();
